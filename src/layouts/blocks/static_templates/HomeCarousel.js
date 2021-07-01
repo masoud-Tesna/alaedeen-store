1fbb537fb6ff75a1dfaa81c0d4dcc67fccb3cc51
Masoud/ <masoud_tesna@live.com>
2021-07-01 15:18:56 +0430

    HomeCarousel.js for mobile DONE...

--- src/layouts/blocks/static_templates/HomeCarousel.js
+++ src/layouts/blocks/static_templates/HomeCarousel.js
@@ -5,20 +5,25 @@ import sofa from "../../../assets/images/sofa.png";
 
 // import ANT Design Components:
 import { Col, Row, Carousel  } from "antd";
+import { useWindowSize } from "../../../functions";
 
 const HomeCarousel = () => {
+  const { width } = useWindowSize();
+
+  const imageHeight = width >= 768 ? 441 : 166;
+
   return (
     <Row className="homeCarousel--container">
       <Col className="homeCarousel--content" span={24}>
         <Carousel className="homeCarousel--items" autoplay>
           <div className="homeCarousel--item">
-            <img className="d-block w-100" src={"https://picsum.photos/1600/441"} alt="First slide" />
+            <img className="d-block w-100" src={`https://picsum.photos/1600/${imageHeight}`} alt="First slide" />
           </div>
           <div className="homeCarousel--item">
-            <img className="d-block w-100" src={"https://picsum.photos/1600/441"} alt="Second slide" />
+            <img className="d-block w-100" src={`https://picsum.photos/1600/${imageHeight}`} alt="Second slide" />
           </div>
           <div className="homeCarousel--item">
-            <img className="d-block w-100" src={"https://picsum.photos/1600/441"} alt="Third slide" />
+            <img className="d-block w-100" src={`https://picsum.photos/1600/${imageHeight}`} alt="Third slide" />
           </div>
         </Carousel>
       </Col>
