9f7b1e15b39d5e82d2d55354aec065c7012a9ba4
Masoud/ <masoud_tesna@live.com>
2021-07-07 01:49:00 +0430

    another about section design DONE...

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,11 @@
 // import style file:
 import './styles/About.less';
 
-// import And Design Co,ponents used:
+// import And Design Components used:
 import { Col, Row } from "antd";
 
 import { useTranslation } from "react-i18next";
-import { __ } from "../functions/Helper";
+import { __, useSetLoaded } from "../functions/Helper";
 import ShowVideoEmbed from "../layouts/blocks/static_templates/ShowVideoEmbed";
 
 // import certificate images:
@@ -13,9 +13,21 @@ import certificate1 from '../assets/images/certificate1.png';
 import certificate2 from '../assets/images/certificate2.png';
 import certificate3 from '../assets/images/certificate3.png';
 import certificate4 from '../assets/images/certificate4.png';
+import { useEffect, useState } from "react";
+import LoadSpinner from "../layouts/blocks/static_templates/LoadSpinner";
 
 const About = () => {
   const { t } = useTranslation();
+
+  const { isLoaded } = useSetLoaded();
+
+  useEffect(() => {
+    document.getElementById('HornStoreApp').scrollIntoView()
+  }, []);
+
+  if (isLoaded) {
+    return <LoadSpinner spinner={'default'} spinnerColor={'#2e8339'} />
+  }
 
   return (
     <Row className="mt-4 about--container">
@@ -152,6 +164,67 @@ const About = () => {
                 </Row>
               </div>
             </div>
+          </Col>
+        </Row>
+      </Col>
+
+      <Col span={24} className="mt-5 about--another__section">
+        <Row gutter={[0, 32]}>
+          <Col className="cursor-pointer about--another__item about--rAndD__container" span={24}>
+            <div className="about--another__bgImage" />
+            <Row className="about--rAndD__content h-100" justify={"space-between"}>
+              <Col className="pl-4 my-auto">
+                <Row>
+                  <Col className="mb-3 vv-font-size-1-8 text-white about--another__title" span={24}>
+                    { t(__('R&D Capability')) }
+                  </Col>
+                  <Col className="vv-font-size-1-4 text-white about--another__viewMore" span={24}>
+                    { t(__('View More R&D Capability')) }
+                  </Col>
+                </Row>
+              </Col>
+              <Col className="my-auto pr-4 about--another__icon">
+                <i className="fa fa-chevron-right vv-font-size-2 text-white" />
+              </Col>
+            </Row>
+          </Col>
+
+          <Col className="cursor-pointer about--another__item about--qualityControl__container" span={24}>
+            <div className="about--another__bgImage" />
+            <Row className="about--qualityControl__content h-100" justify={"space-between"}>
+              <Col className="pl-4 my-auto">
+                <Row>
+                  <Col className="mb-3 vv-font-size-1-8 text-white about--another__title" span={24}>
+                    { t(__('Quality Control')) }
+                  </Col>
+                  <Col className="vv-font-size-1-4 text-white about--another__viewMore" span={24}>
+                    { t(__('View More Quality Control')) }
+                  </Col>
+                </Row>
+              </Col>
+              <Col className="my-auto pr-4 about--another__icon">
+                <i className="fa fa-chevron-right vv-font-size-2 text-white" />
+              </Col>
+            </Row>
+          </Col>
+
+          <Col className="cursor-pointer about--another__item about--exportCapability__container" span={24}>
+            <div className="about--another__bgImage" />
+            <Row className="about--exportCapability__content h-100" justify={"space-between"}>
+              <Col className="pl-4 my-auto">
+                <Row>
+                  <Col className="mb-3 vv-font-size-1-8 text-white about--another__title" span={24}>
+                    { t(__('Export Capability')) }
+                  </Col>
+                  <Col className="vv-font-size-1-4 text-white about--another__viewMore" span={24}>
+                    { t(__('View More Export Capability')) }
+                  </Col>
+                </Row>
+              </Col>
+              <Col className="my-auto pr-4 about--another__icon">
+                <i className="fa fa-chevron-right vv-font-size-2 text-white" />
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Col>
