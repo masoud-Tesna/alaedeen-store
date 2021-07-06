e32f8f505f0323a48d2f1753040daf8818654a39
Masoud/ <masoud_tesna@live.com>
2021-07-06 23:20:20 +0430

    Certificate section Design DONE...

--- src/components/About.js
+++ src/components/About.js
@@ -8,6 +8,12 @@ import { useTranslation } from "react-i18next";
 import { __ } from "../functions/Helper";
 import ShowVideoEmbed from "../layouts/blocks/static_templates/ShowVideoEmbed";
 
+// import certificate images:
+import certificate1 from '../assets/images/certificate1.png';
+import certificate2 from '../assets/images/certificate2.png';
+import certificate3 from '../assets/images/certificate3.png';
+import certificate4 from '../assets/images/certificate4.png';
+
 const About = () => {
   const { t } = useTranslation();
 
@@ -97,6 +103,53 @@ const About = () => {
 
               <div className="about--video">
                 <ShowVideoEmbed embedLink="https://www.aparat.com/video/video/embed/videohash/A8HYE/vt/frame?isamp" extraClassName={'aboutPage'} embedClassName = "rounded-10" />
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Col>
+
+      <Col span={24} className="mt-5 about--certificates__section">
+        <Row>
+          <Col span={24} className="vv-font-size-1-8 font-weight-bold text-47 about--title">
+            { t(__('Certificate')) }
+          </Col>
+          <Col span={24} className="mt-3 about--imagesSection">
+            <div className="about--images">
+              <div className="about--image">
+                <Row>
+                  <Col className = "about--imageBackground" span={24} style={{
+                    backgroundImage: `url(${certificate1})`
+                  }} />
+                  <Col className = "m-0 p-0 vv-font-size-1-6 text-center text-47 mt-3" xs={24}>CoC</Col>
+                </Row>
+              </div>
+
+              <div className="about--image">
+                <Row>
+                  <Col className = "about--imageBackground" span={24} style={{
+                    backgroundImage: `url(${certificate2})`
+                  }} />
+                  <Col className = "m-0 p-0 vv-font-size-1-6 text-center text-47 mt-3" xs={24}>ISO 9001</Col>
+                </Row>
+              </div>
+
+              <div className="about--image">
+                <Row>
+                  <Col className = "about--imageBackground" span={24} style={{
+                    backgroundImage: `url(${certificate3})`
+                  }} />
+                  <Col className = "m-0 p-0 vv-font-size-1-6 text-center text-47 mt-3" xs={24}>ISIRI</Col>
+                </Row>
+              </div>
+
+              <div className="about--image">
+                <Row>
+                  <Col className = "about--imageBackground" span={24} style={{
+                    backgroundImage: `url(${certificate4})`
+                  }} />
+                  <Col className = "m-0 p-0 vv-font-size-1-6 text-center text-47 mt-3" xs={24}>CoC</Col>
+                </Row>
               </div>
             </div>
           </Col>
