d302a99fefe75697cb4e44c56d31544097a48c23
Masoud/ <masoud_tesna@live.com>
2021-07-30 16:26:13 +0430

    Add Domain for access StorageLocal...

--- src/components/accessStoragePage.js
+++ src/components/accessStoragePage.js
@@ -8,6 +8,14 @@ let accessStoragePage = createHost([
   {
     origin: 'http://localhost:3001',
     allowedMethods: ['get', 'set', 'remove'],
+  },
+  {
+    origin: 'https://hornb2b.com',
+    allowedMethods: ['get', 'set', 'remove'],
+  },
+  {
+    origin: 'https://store.hornb2b.com',
+    allowedMethods: ['get', 'set', 'remove'],
   }
 ]);
 
