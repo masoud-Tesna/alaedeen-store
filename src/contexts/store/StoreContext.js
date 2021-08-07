181aae165cc1a494cd0e947a5da2faffa606afe7
Masoud/ <masoud_tesna@live.com>
2021-08-07 16:31:07 +0430

    add isLoading

--- src/contexts/store/StoreContext.js
+++ src/contexts/store/StoreContext.js
@@ -13,29 +13,32 @@ function StoreProvider({ children }) {
   const store_id_query_string = url.searchParams.get('store_id');
 
   // useState For Store use in app
-  const [store, setStore] = useState({status: 'loading', id: store_id_query_string, name: null, brand: null, email: null, logo: null});
+  const [store, setStore] = useState({isLoading: true, status: 'loading', id: store_id_query_string, name: null, brand: null, email: null, logo: null});
 
 
   const store_id = window.localStorage.getItem("store_id") || store_id_query_string ;
 
-  const { data } = useGetApi(`store-check-api`, `store_id=${store_id}`, `storeCheck_${store_id}`);
-
+  const { data, isLoading } = useGetApi(`store-check-api`, `store_id=${store_id}`, `storeCheck_${store_id}`);
 
   useEffect(() => {
+
+    setStore(prevState => {
+      return {...prevState, isLoading: true}
+    });
 
     if (data) {
       if (data.status === 'D') {
         setStore(prevState => {
-          return {...prevState, status: 'disable'}
+          return {...prevState, isLoading: isLoading, status: 'disable'}
         });
       }else if (data.status === 'A') {
         setStore(prevState => {
-          return {...prevState, status: 'active', id: store_id, name: data.name, brand: data.brand, email: data.email, logo: data.logo}
+          return {...prevState, isLoading: isLoading, status: 'active', id: store_id, name: data.name, brand: data.brand, email: data.email, logo: data.logo}
         });
       }
     }
 
-  }, [data]);
+  }, [data, isLoading]);
 
   return (
     <storeContext.Provider value={ store }>
