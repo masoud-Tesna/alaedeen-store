96228a644fb64db905acf6ed7d2cd47c0be6a36a
Masoud/ <masoud_tesna@live.com>
2021-08-21 07:34:38 +0430

    get footer pages from API...

--- src/layouts/footer/default/DefaultFooter.js
+++ src/layouts/footer/default/DefaultFooter.js
@@ -9,8 +9,15 @@ import { Row, Col } from 'antd';
 import { __ } from '../../../functions/Helper';
 
 import { useTranslation } from "react-i18next";
+import { useGetApi } from "../../../functions";
+import { useGetStoreState } from "../../../contexts/store/StoreContext";
+import { Link } from "react-router-dom";
 
 const DefaultFooter = () => {
+
+  const { id: storeId } = useGetStoreState();
+
+  const { isLoading, data: pages } = useGetApi(`footer-pages-api`, `store_id=${storeId}`, `footerPage_${storeId}`);
 
   const { t } = useTranslation();
 
@@ -18,12 +25,15 @@ const DefaultFooter = () => {
     <Row className="bg-f6 pt-md-5 footer--container" justify={"center"}>
       <Col span={24} className="footer--content">
         <Row className="row-cols-2 row-cols-md-3" gutter={[6, 24]}>
-          <Col className="text-47">{ t(__('Order registration guide')) }</Col>
-          <Col className="text-47">{ t(__('Warranty terms')) }</Col>
-          <Col className="text-47">{ t(__('Privacy')) }</Col>
-          <Col className="text-47 text-uppercase">{ t(__('FAQ')) }</Col>
-          <Col className="text-47">{ t(__('Terms and Conditions')) }</Col>
-          <Col className="text-47">{ t(__('Download')) }</Col>
+          {pages?.pages?.map(page => {
+            return(
+              <Col key={`footerPage_${page.page_id}`} className="text-47">
+                <Link to= {`/page/${page.page_id}`}>
+                  { page.page}
+                </Link>
+              </Col>
+            );
+          })}
         </Row>
       </Col>
     </Row>
