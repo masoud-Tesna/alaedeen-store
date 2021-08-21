41a185969573d4f26eb6d891fd273aa44d9509eb
Masoud/ <masoud_tesna@live.com>
2021-08-21 14:50:07 +0430

    get footer pages from API...

--- src/layouts/footer/default/DefaultFooter.js
+++ src/layouts/footer/default/DefaultFooter.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 // import Styles For default:
 import './styles.less';
 
@@ -6,12 +8,8 @@ import { Row, Col } from 'antd';
 
 
 // import helper functions:
-import { __ } from '../../../functions/Helper';
-
-import { useTranslation } from "react-i18next";
 import { useGetApi } from "../../../functions";
 import { useGetStoreState } from "../../../contexts/store/StoreContext";
-import { Link } from "react-router-dom";
 
 const DefaultFooter = () => {
 
@@ -19,16 +17,14 @@ const DefaultFooter = () => {
 
   const { isLoading, data: pages } = useGetApi(`footer-pages-api`, `store_id=${storeId}`, `footerPage_${storeId}`);
 
-  const { t } = useTranslation();
-
   return (
     <Row className="bg-f6 pt-md-5 footer--container" justify={"center"}>
       <Col span={24} className="footer--content">
         <Row className="row-cols-2 row-cols-md-3" gutter={[6, 24]}>
-          {pages?.pages?.map(page => {
+          {pages?.map(page => {
             return(
               <Col key={`footerPage_${page.page_id}`} className="text-47">
-                <Link to= {`/page/${page.page_id}`}>
+                <Link to= {`/page/${page.seo_name}`}>
                   { page.page}
                 </Link>
               </Col>
