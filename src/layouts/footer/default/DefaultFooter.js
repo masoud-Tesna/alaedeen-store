89c7ded81ddb1a934e584d861ba4261e2848c25b
Masoud/ <masoud_tesna@live.com>
2021-10-06 07:28:51 +0330

    add loading skeleton for page list show in footer

--- src/layouts/footer/default/DefaultFooter.js
+++ src/layouts/footer/default/DefaultFooter.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import './styles.less';
 
 // Ant Design Import:
-import { Row, Col } from 'antd';
+import { Row, Col, Skeleton } from 'antd';
 
 
 // import helper functions:
@@ -21,6 +21,21 @@ const DefaultFooter = () => {
     <Row className="bg-f6 pt-md-5 footer--container" justify={"center"}>
       <Col span={24} className="footer--content">
         <Row className="row-cols-2 row-cols-md-3" gutter={[6, 24]}>
+
+          {isLoading &&
+            <>
+              <Col className="text-47">
+                <Skeleton.Input style={{ width: 140 }} active={true} size={"small"} />
+              </Col>
+              <Col className="text-47">
+                <Skeleton.Input style={{ width: 130 }} active={true} size={"small"} />
+              </Col>
+              <Col className="text-47">
+                <Skeleton.Input style={{ width: 120 }} active={true} size={"small"} />
+              </Col>
+            </>
+          }
+
           {pages?.map(page => {
             return(
               <Col key={`footerPage_${page.page_id}`} className="text-47">
