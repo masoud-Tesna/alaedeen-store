b15dc963ab42297a7a6cf85e9a486a7aae7f3564
Masoud/ <masoud_tesna@live.com>
2021-12-01 03:05:37 +0330

    fix: mobile menu

--- src/layouts/header/default/DefaultHeader.js
+++ src/layouts/header/default/DefaultHeader.js
@@ -159,14 +159,14 @@ const DefaultHeader = () => {
               <Col span={24} className="menuXs--bgTopSection__container">
                 <Row className="h-100 px-4" align="middle" gutter={16}>
 
-                  { user_data.load ?
+                  { user_data?.load ?
                     <>
                       <Skeleton avatar paragraph={{ rows: 1 }} />
                     </> :
                     <>
-                      { user_data.auth.user_id ?
+                      { user_data?.auth?.user_id ?
                         <>
-                          <Col span={ user_data.auth.company_logo ? 7 : 5 }>
+                          <Col flex={ user_data?.auth?.company_logo ? "76px" : "51px" }>
                             { user_data.auth.company_logo ?
                               <span className="content--account__companyLogo">
                                       <img src={user_data.auth.company_logo.logo_path} alt=""/>
@@ -175,7 +175,7 @@ const DefaultHeader = () => {
                             }
                           </Col>
 
-                          <Col span={ user_data.auth.company_logo ? 17 : 19 }>
+                          <Col flex="1 1">
                             <Row gutter={[0, 5]}>
                               <Col span={24} className="text-white vv-font-size-1-7 font-weight-bolder">
                                 {t(__('Hello'))}
@@ -198,11 +198,11 @@ const DefaultHeader = () => {
                           </Col>
                         </> :
                         <>
-                          <Col>
+                          <Col flex="51px">
                             <i className="fal fa-user text-white vv-font-size-4-5" />
                           </Col>
 
-                          <Col>
+                          <Col flex="1 1">
                             <Row gutter={[0, 5]}>
                               <Col span={24} className="text-white vv-font-size-1-7 font-weight-bolder">
                                 {t(__('Hello'))}
@@ -359,7 +359,7 @@ const DefaultHeader = () => {
                     {t(__('Download the Horn app'))}
                   </div>
                   <div className="text-white vv-font-size-1-2 mt-2">
-                    {t(__('For 10x Faster'))}
+                    {t('app_msg.mobile_menu')}
                   </div>
                 </div>
               </Space>
